dc025389f5524d3c63b69f33338a4bc3dd1bf654
kabradshaw1 <itllbok82@gmail.com>
2023-01-23 20:56:20 -0500

    working on integrating react and django

--- django_postgres_react/client/src/App.js
+++ django_postgres_react/client/src/App.js
@@ -1,16 +1,13 @@
-
 import React, { Component } from 'react';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Book from "./Component/Book";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route path="/" exact component={Book} />
-    </Router>
+    <Book/>
     );
   }
 }
