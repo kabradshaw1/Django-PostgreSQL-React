4c52fbfad8f08751bc0374b4416331eaec311d93
kabradshaw1 <itllbok82@gmail.com>
2023-01-23 21:20:40 -0500

    trying to write my own component for book

--- django_postgres_react/client/src/Component/Book/index.js
+++ django_postgres_react/client/src/Component/Book/index.js
@@ -1,37 +1,11 @@
-import React, { Component } from "react";
-
+import React, { useState } from "react";
 import axios from "axios";
 
-export default class Book extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    books:[],
-    };
-    this.loadBooks = this.loadBooks.bind(this);
-  }
+const Book = () => {
 
-  componentWillMount() {
-    this.loadBooks();
-  }
-
-  async loadBooks()
-  {
-    const promise = await axios.get("http://localhost:8000/book/");
-    const status = promise.status;
-    if(status===200)
-    {
-      const data = promise.data.data;
-      this.setState({books:data});
-    }
-  }
-
-  render() {
-    return(
-      <div>
-        <h1>Books</h1>
-            {this.state.books.map((value,index)=>{return <h4 key={index}>{value}</h4>})}
-      </div>
-    )
-  }
+  return(
+    <div></div>
+  )
 }
+
+export default Book;
